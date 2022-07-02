3de7c355ebf2ecd8d063d8237685e35882653eb2
joseyzambranov <joseyzambranovpe@gmail.com>
2022-07-01 23:05:46 -0500

    fixed error login on table

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -39,7 +39,7 @@ export default function Header(){
            </div>
            <LoginLogout onClick={handleClick}  >
            
-            {user?<><i class='bx bx-log-out-circle'></i></>:<><Link className="text-white" to="Login">
+            {user?<><i class='bx bx-log-out-circle'></i></>:<><Link className="text-white" to="/Login">
            <i class='bx bx-log-in-circle'></i>
             </Link></>
           }
